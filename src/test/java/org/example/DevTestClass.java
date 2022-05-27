75d85404c0a3b6fc02c5a3384ae8f3e1590fe26d
nitinmore <nitinmore111@gmail.com>
2022-05-27 13:38:25 +0530

    second commit

--- src/test/java/org/example/DevTestClass.java
+++ src/test/java/org/example/DevTestClass.java
@@ -4,5 +4,6 @@ public class DevTestClass {
 
     public static void main(String[] args) {
         System.out.println("I am from main class of DevTestclass");
+        System.out.println("This is the new line in the existing code");
     }
 }
