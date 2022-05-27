620a64fc9940637084721779b33bd8ad369cc14f
nitinmore <nitinmore111@gmail.com>
2022-05-27 12:17:04 +0530

    initial commit for the change

---
+++ src/main/java/org/example/DevClass.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class DevClass {
+
+
+    public static void main(String[] args) {
+        System.out.println("I am fron dev main class");
+    }
+}
---
+++ src/test/java/org/example/DevTestClass.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class DevTestClass {
+
+    public static void main(String[] args) {
+        System.out.println("I am from main class of DevTestclass");
+    }
+}
